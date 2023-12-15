3cdc28366ad73fdc8ed388a3296c5afcdf29b9ac
QuangMinh1411 <102689124+QuangMinh1411@users.noreply.github.com>
2023-12-15 14:51:02 +0700

    Generic

---
+++ Generic/index.ts
@@ -0,0 +1,43 @@
+interface Cat {
+    name:string;
+    breed:string;
+}
+
+function identity<T>(item:T):T{
+    return item;
+}
+
+function getRandomElement<T>(list:T[]):T {
+    return list[Math.floor(Math.random()*list.length)]
+}
+
+// identity<string>("Hi");
+// identity<boolean>(true);
+// identity<number>(5.4);
+// identity<Cat>()
+
+getRandomElement<string>(["a","b","c"]);
+getRandomElement<number>([2,3,1,5,6]);
+
+function merge<T extends object,V extends object>(obj1:T,obj2:V){
+    return {
+        ...obj1,
+        ...obj2
+    }
+}
+const comboObj = merge({name:'Scort'},{pets:["blue","Elton"]});
+console.log(comboObj);
+
+interface Lengthy {
+    length:number;
+}
+
+function printDoubleLength<T extends Lengthy>(thing:T):number{
+    return thing.length*2;
+}
+
+printDoubleLength("Hello");
+
+function makeEmptyArray<T=number>():T[]{
+    return [];
+}
